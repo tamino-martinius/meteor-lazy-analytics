410248001897055cabc782ff799781e82725d8e4
Dan Dascalescu <ddascalescu+github@gmail.com>
2015-09-05 14:06:36 -0700

    Fix typo but this needs a more precise description

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'zaku:lazy-analytics',
   version: '0.0.1',
-  summary: 'Load analycis with js',
+  summary: 'Load analytics from JavaScript',
   git: 'https://github.com/zaku-eu/meteor-lazy-analytics.git',
   documentation: 'README.md'
 });
